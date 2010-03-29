64c3a0f09be7ee9578940241988538cb631fd4bb
David Mudrak <david.mudrak@gmail.com>
2010-03-29 20:44:00 +0000

    MDL-21694 Moving repositories strings into plugin space

---
+++ lang/en_utf8/repository_boxnet.php
@@ -0,0 +1,14 @@
+<?php
+$string['apikey'] = 'API Key';
+$string['boxnet:view'] = 'View box.net repository';
+$string['configplugin'] = 'Box.net configuration';
+$string['information'] = '<div>Get <a href=\"http://enabled.box.net/my-projects/\">Box.net API Key</a> for your Moodle site. </div>';
+$string['invalidpassword'] = 'Invalid password';
+$stirng['invalidtoken'] = 'Invalid authentication token';
+$string['nullfilelist'] = 'There are no files in this repository';
+$string['password'] = 'Password';
+$string['repositorydesc'] = 'Repository on Box.net';
+$string['repositoryname'] = 'Box.net';
+$string['saved'] = 'Box.net data saved';
+$string['shareurl'] = 'Share URL';
+$string['username'] = 'Username for Box.net';
